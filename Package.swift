a394231fd3cdee187862f7f85751726b7fd1ae4e
Sanjay Yepuri <sanjay@Sanjays-MacBook-Pro.local>
2018-10-12 02:56:55 -0500

    Fixed manifest to work on linux

--- Package.swift
+++ Package.swift
@@ -14,9 +14,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Ir",
-            dependencies: ["Utils"]),
+            dependencies: ["Utils"],
+            path: "Sources/ir"),
+
         .target(
-            name: "Utils"),
+            name: "Utils",
+            path: "Sources/utils"),
+        
         .testTarget(
             name: "swiftsearchengineTests",
             dependencies: ["Ir", "Utils"]),
